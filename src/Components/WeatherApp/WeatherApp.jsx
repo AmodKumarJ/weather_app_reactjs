745555973e6e3065186f6a64920ae919f3d24eb5
AmodkumarJ <amodkumarjg@gmail.com>
2023-11-27 10:42:52 +0530

    Small Changes

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -3,9 +3,9 @@ import './WeatherApp.css'
 import search_icon from '../Assets/search.png'
 import clear_icon from '../Assets/clear.png'
 import cloud_icon from '../Assets/cloud.png'
-import drizzle_icon from '../Assets/drizzle.png'
-import rain_icon from '../Assets/rain.png'
-import snow_icon from '../Assets/snow.png'
+// import drizzle_icon from '../Assets/drizzle.png'
+// import rain_icon from '../Assets/rain.png'
+// import snow_icon from '../Assets/snow.png'
 import wind_icon from '../Assets/wind.png'
 import humidity_icon from '../Assets/humidity.png' 
 const WeatherApp = () => {
@@ -25,8 +25,8 @@ const WeatherApp = () => {
         const temprature = document.getElementsByClassName("weather-temp")
         const location = document.getElementsByClassName("weather-location")
 
-        humidity[0].innerHTML = data.main.humidity
-        wind[0].innerHTML = data.wind.speed
+        humidity[0].innerHTML = data.main.humidity+"%"
+        wind[0].innerHTML = data.wind.speed+"Km/h"
         temprature[0].innerHTML = data.main.temp+"°C"
         location[0].innerHTML = data.name
     }
